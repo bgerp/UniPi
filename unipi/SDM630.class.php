2d9c6af497f11d604e453eb61928ad11d646029d
Orlin Dimitrov <orlin369@gmail.com>
2018-06-15 15:19:08 +0300

    bgERP драйвер класове

--- unipi/SDM630.class.php
+++ unipi/SDM630.class.php
@@ -9,32 +9,24 @@
  * @copyright 2018 POLYGONTeam OOD
  * @license   GPL 3
  * @since     v 0.1
- * @title     Eлектромер Eastrongroup SDM120
- * @see       https://bg-etech.de/download/manual/SDM120CT-Modbus.pdf
+ * @title     Eлектромер Eastrongroup SDM630
+ * @see       https://bg-etech.de/download/manual/SDM630Register1-5.pdf
  */
-class eastrongroup_SDM120 extends sens2_ProtoDriver
+class eastrongroup_SDM630 extends sens2_ProtoDriver
 {
     /**
      * Заглавие на драйвера
      */
-    var $title = 'EASTRONGROUP_SDM120';
+    var $title = 'EASTRONGROUP_SDM630';
     
     
     /**
      * Описание на входовете
      */
     var $inputs = array(
-        'Voltage'              => array('caption'=>'Напрежение', 'uom' => 'V'),
-        'Current'              => array('caption'=>'Ток', 'uom' => 'A'),
-        'ActivePower'          => array('caption'=>'Активна енергия', 'uom' => 'W'),
-        'ApparentPower'        => array('caption'=>'Явна енергия', 'uom' => 'VA'),
-    	'ReactivePower'        => array('caption'=>'Реактивна енергия', 'uom' => 'WAr'),
-        'PowerFactor'          => array('caption'=>'Косинус Фи', 'uom'=>'Deg'),
-        'Frequency'            => array('caption'=>'Честота', 'uom'=>'Hz'),
-        'ImportActiveEnergy'   => array('caption'=>'Вх. активна енергия', 'uom'=>'KWh'),
-        'ExportActiveEnergy'   => array('caption'=>'Изх. активна енергия', 'uom'=>'KWh'),
-        'ImportReactiveEnergy' => array('caption'=>'Вх. реактивна енергия', 'uom'=>'kvarh'),
-        'ExportReactiveEnergy' => array('caption'=>'Изх. реактивна енергия', 'uom'=>'kvarh'),
+        'Phase1LineToNeutralVolts' => array('caption'=>'Напрежение L1 към N', 'uom' => 'V'),
+        'Phase2LineToNeutralVolts' => array('caption'=>'Напрежение L2 към N', 'uom' => 'V'),
+        'Phase3LineToNeutralVolts' => array('caption'=>'Напрежение L3 към N', 'uom' => 'V'),
     );
     
     /**
@@ -80,28 +72,19 @@ class eastrongroup_SDM120 extends sens2_ProtoDriver
         $neuron = new Neuron($config->ip, $config->port);
         
         // Създай уред.
-        $sdm120 = new SDM120($neuron, $config->unit); 
+        $sdm630 = new SDM630($neuron, $config->unit); 
         // Вземи данните от уреда.
-        $sdm120->Update();
+        $sdm630->Update();
         
         // Прочитаме изчерпаната до сега мощност
-        $res['Voltage'] = $sdm120->getVoltage();
-        $res['Current'] = $sdm120->getCurrent();
-        $res['ActivePower'] = $sdm120->getActivePower();
-        $res['ApparentPower'] = $sdm120->getApparentPower();
-        $res['ReactivePower'] = $sdm120->getReactivePower();
-        $res['PowerFactor'] = $sdm120->getPowerFactor();
-        $res['Frequency'] = $sdm120->getFrequency();
-        $res['ImportActiveEnergy'] = $sdm120->getImportActiveEnergy();
-        $res['ExportActiveEnergy'] = $sdm120->getExportActiveEnergy();
-        $res['ImportReactiveEnergy'] = $sdm120->getImportActiveEnergy();
-        $res['ExportReactiveEnergy'] = $sdm120->getExportReactiveEnergy();
+        $res['Phase1LineToNeutralVolts'] = $sdm630->getPhase1Voltage();
+        $res['Phase2LineToNeutralVolts'] = $sdm630->getPhase1Voltage();
+        $res['Phase3LineToNeutralVolts'] = $sdm630->getPhase1Voltage();
         
         if (empty($addresses)) {
             return "Грешка при четене от {$config->ip}";
         }
                 
         return $res;
-    }
-    
+    } 
 }
