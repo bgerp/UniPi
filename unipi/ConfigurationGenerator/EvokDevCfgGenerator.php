92029e74006aa3f302feed6a2bb1cb8a559c905a
Orlin Dimitrov <orlin369@gmail.com>
2018-07-13 13:41:33 +0300

    Добавена документация

--- unipi/ConfigurationGenerator/EvokDevCfgGenerator.php
+++ unipi/ConfigurationGenerator/EvokDevCfgGenerator.php
@@ -8,12 +8,20 @@ class EvokDevCfgGenerator
 
     #region Variables
 
-    private $content = array();
+    /**
+     * Device registers.
+     *
+     * @var array
+     */
+    private $configuration = array();
 
     #endregion
 
     #region Construcotr
 
+    /**
+     * EvokDevCfgGenerator constructor.
+     */
     public function __construct()
     {
     }
@@ -23,22 +31,37 @@ class EvokDevCfgGenerator
     #region Getters and Setters
 
     /**
+     * Returns the configuration array.
+     *
      * @return array
      */
-    public function generateConfiguration()
+    public function getConfiguration()
     {
-        return $this->content;
+        return $this->configuration;
     }
 
     #endregion
 
     #region Public Methods
 
+    /**
+     * Add device tyep (name).
+     *
+     * @param $type
+     */
     public function addType($type)
     {
-        $this->content['type'] = $type;
+        $this->configuration['type'] = $type;
     }
 
+    /**
+     * Add mmodbus register block.
+     *
+     * @param integer $board_index Board index in the bus.
+     * @param integer $start_reg Starting register.
+     * @param integer $count Quantyty of the registers.
+     * @param integer int $frequency Update frequency.
+     */
     public function addModbusRegisterBlocks($board_index, $start_reg, $count, $frequency = 60)
     {
         $block = array();
@@ -48,14 +71,22 @@ class EvokDevCfgGenerator
         $block['count'] = $count;
         $block['frequency'] = $frequency;
 
-        if(!isset($this->content['modbus_register_blocks']))
+        if(!isset($this->configuration['modbus_register_blocks']))
         {
-            $this->content['modbus_register_blocks'] = array();
+            $this->configuration['modbus_register_blocks'] = array();
         }
 
-        array_push($this->content['modbus_register_blocks'], $block);
+        array_push($this->configuration['modbus_register_blocks'], $block);
     }
 
+    /**
+     * Add mmodbus register feture.
+     *
+     * @param string $type Register feature type.
+     * @param integer $major_group Major group.
+     * @param integer $count Quantyty of the registers.
+     * @param integer $start_reg Starting register.
+     */
     public function addModbusFeatures($type, $major_group, $count, $start_reg)
     {
         $feture = array();
@@ -65,14 +96,24 @@ class EvokDevCfgGenerator
         $feture['count'] = $count;
         $feture['start_reg'] = $start_reg;
 
-        if(!isset($this->content['modbus_features']))
+        if(!isset($this->configuration['modbus_features']))
         {
-            $this->content['modbus_features'] = array();
+            $this->configuration['modbus_features'] = array();
         }
 
-        array_push($this->content['modbus_features'], $feture);
+        array_push($this->configuration['modbus_features'], $feture);
     }
 
+    /**
+     * Create configuration from registers data.
+     *
+     * @param string $type
+     * @param integer $board_index Board index in the bus.
+     * @param $frequency Update frequency.
+     * @param $major_group Major group.
+     * @param $regsters Modbus registers.
+     * @return array Settings
+     */
     public static function createFromRegisters($type, $board_index, $frequency, $major_group, $regsters)
     {
         $configuration = new EvokDevCfgGenerator();
@@ -87,7 +128,7 @@ class EvokDevCfgGenerator
             $configuration->addModbusFeatures('REGISTER', $major_group, $addresses_count, $addresses[0]);
         }
 
-        return $configuration->generateConfiguration();
+        return $configuration->getConfiguration();
     }
 
     #endregion
