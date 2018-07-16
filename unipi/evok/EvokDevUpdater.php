217ca8a152cfa67e79974f0c80771333ec63d380
Orlin Dimitrov <orlin369@gmail.com>
2018-07-16 13:28:00 +0300

    Добавена документация

--- unipi/evok/EvokDevUpdater.php
+++ unipi/evok/EvokDevUpdater.php
@@ -8,12 +8,35 @@
 
 class EvokDevUpdater
 {
+
+    #region Variables
+
+    /**
+     * @var string $ip IP address of the device.
+     */
     private $ip = '';
+
+    /**
+     * @var integer $port Port of the device.
+     */
     private $port = '';
+
+    /**
+     * @var string $user User of the device.
+     */
     private $user = '';
+
+    /**
+     * @var string $password Password of the device.
+     */
     private $password = '';
 
+    /**
+     * @var string $remote_hardware_definiotion_path Definitions path.
+     */
     private $remote_hardware_definiotion_path = '/etc/hw_definitions/';
+
+    #endregion
 
     public function __construct($ip, $port, $user, $password)
     {
@@ -37,39 +60,63 @@ class EvokDevUpdater
             $this->remote_hardware_definiotion_path.$file_name,
             0644);
 
-        //$sftp = ssh2_sftp($connection);
+        // Exit.
+        ssh2_exec(
+            $connection,
+            'exit');
 
-        // Create a new folder
-        //ssh2_sftp_mkdir($sftp, './test_folder');
-
+        //
         unset($connection);
+
     }
 
     public function removeHardwareDefinition($file_name)
     {
-        //$file_name = pathinfo($file_path)['basename'];
-
         $connection = ssh2_connect($this->ip, $this->port);
 
         ssh2_auth_password($connection, $this->user, $this->password);
 
         $sftp = ssh2_sftp($connection);
 
-        //ssh2_sftp_unlink($sftp, '/home/username/myfile');
+        ssh2_sftp_unlink(
+            $sftp,
+            $this->remote_hardware_definiotion_path.$file_name);
 
+        // Exit.
+        ssh2_exec(
+            $connection,
+            'exit');
+
+        //
         unset($connection);
+
     }
 
     public function restartService()
     {
+        //
         $connection = ssh2_connect($this->ip, $this->port);
 
+        //
         ssh2_auth_password($connection, $this->user, $this->password);
 
-        $stop_stream = ssh2_exec($connection, 'systemctl stop evok');
+        // Stop the service.
+        ssh2_exec(
+            $connection,
+            'systemctl stop evok');
 
-        $start_stream = ssh2_exec($connection, 'sudo python /opt/evok/evok.py');
+        // Start the service.
+        ssh2_exec(
+            $connection,
+            'sudo python /opt/evok/evok.py > /dev/null 2>&1 &');
 
+        // Exit.
+        ssh2_exec(
+            $connection,
+            'exit');
+
+        //
         unset($connection);
+
     }
 }
